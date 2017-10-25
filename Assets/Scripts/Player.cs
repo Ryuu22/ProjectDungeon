be75f99220c62b5fc33c37b7e3456607716e3b72
Fernando Diago <fernandodl2435@gmail.com>
2017-10-25 19:01:18 +0200

    Created Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    public float speed;
+
+    public Transform playerTransform;
+    public Vector2 playerVector;
+
 	void Start ()
     {
-		
-	}
+        playerTransform = this.transform;
+
+    }
 
 	void Update ()
     {
-		
+        playerVector.x = playerTransform.transform.position.x;
+        playerVector.y = playerTransform.transform.position.y;		
 	}
+
+    public void PlayerMovement(Vector2 axis)
+    {
+        playerVector.x += axis.x * Time.deltaTime * speed;
+        playerVector.y += axis.y * Time.deltaTime * speed;
+
+        playerTransform.position = playerVector;
+    }
 }
