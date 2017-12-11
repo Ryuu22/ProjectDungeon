52b5700a924d5b71e7c84157125a65b02e4edee2
Fernando Diago <fernandodl2435@gmail.com>
2017-12-11 19:06:00 +0100

    Passive Slime roams the zone
    Passive slime moves trough the zone and damages the player on contact

--- Assets/Scripts/Enemies/PassiveSlime.cs
+++ Assets/Scripts/Enemies/PassiveSlime.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class PassiveSlime : MonoBehaviour
 {
+    [Header("Game Elements")]
+    MoveMaster moveM;
+
     [Header("Slime Fields")]
     [SerializeField]
     int life = 20;
+    Vector2 actualPos;
+    float speed = 1;
+    int damage = 5;
+    float damageCooldown = 2;
+    float damageCounter;
+    float patrolCounter;
+    float patrolMoveCounter;
+    [SerializeField]
+    Vector2 patrolTarget;
     Animator myAnim;
 
     //provisional
@@ -18,20 +30,34 @@ public class PassiveSlime : MonoBehaviour
     enum PassiveSlimeState
     {
         Idle,
+        TargetPatrol,
+        Patrol,
         Dead
     }
 
     void Start ()
     {
+        actualPos = this.transform.position;
+        moveM = GameObject.FindGameObjectWithTag("MoveMaster").GetComponent<MoveMaster>();
         myAnim = GetComponentInChildren<Animator>();
     }
 
 	void Update ()
     {
+        damageCounter -= Time.deltaTime;
+
         switch (currentPassiveSlimeState)
         {
             case PassiveSlimeState.Idle:
                 Idle();
+                break;
+
+            case PassiveSlimeState.TargetPatrol:
+                TargetPatrol();
+                break;
+
+            case PassiveSlimeState.Patrol:
+                Patrol();
                 break;
 
             case PassiveSlimeState.Dead:
@@ -47,7 +73,31 @@ public class PassiveSlime : MonoBehaviour
 
     void Idle()
     {
+        patrolCounter -= Time.deltaTime;
 
+        if(patrolCounter <= 0)
+        {
+            patrolCounter = Random.Range(4, 9);
+            TargetPatrolState();
+        }
+    }
+
+    void TargetPatrol()
+    {
+        patrolTarget = new Vector2(actualPos.x + Random.Range(-3, 3), actualPos.y + Random.Range(-2, 2));
+        PatrolState();
+    }
+
+    void Patrol()
+    {
+        patrolMoveCounter += Time.deltaTime;
+        moveM.Move(this.gameObject, patrolTarget, speed);
+
+        if(Vector2.Distance(this.transform.position, patrolTarget) < 1 || patrolMoveCounter > 5)
+        {
+            patrolMoveCounter = 0;
+            IdleState();
+        }
     }
 
     void Dead()
@@ -70,6 +120,16 @@ public class PassiveSlime : MonoBehaviour
         currentPassiveSlimeState = PassiveSlimeState.Idle;
     }
 
+    void TargetPatrolState()
+    {
+        currentPassiveSlimeState = PassiveSlimeState.TargetPatrol;
+    }
+
+    void PatrolState()
+    {
+        currentPassiveSlimeState = PassiveSlimeState.Patrol;
+    }
+
     void DeadState()
     {
         currentPassiveSlimeState = PassiveSlimeState.Dead;
@@ -88,15 +148,22 @@ public class PassiveSlime : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void Damage(GameObject player)
+    {
+        if(damageCounter <= 0)
+        {
+            damageCounter = damageCooldown;
+            player.GetComponent<Player>().RecieveDamage(damage);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("Passive Slime Touched" + other);
 
         if (other.gameObject.tag == "Player")
         {
-            other.GetComponent<Player>().RecieveDamage(life);
+            Damage(other.gameObject);
         }
-
-        DeadState();
     }
 }
