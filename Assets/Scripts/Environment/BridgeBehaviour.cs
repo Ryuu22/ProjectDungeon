106fed1561f14b362afff8de245ba1487b8b9f6f
Ryuu <Daniicarmona22@gmail.com>
2017-12-14 18:04:54 +0100

    Bridge Ready for Alpha.
    +Edited BridgeBehaviour.cs
    +Created Bridge Prefab

--- Assets/Scripts/Environment/BridgeBehaviour.cs
+++ Assets/Scripts/Environment/BridgeBehaviour.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class BridgeBehaviour : MonoBehaviour {
 
+    [SerializeField] float timeToLive;
+
+    [Header("Game Objects")]
+
+    [SerializeField] GameObject graphic;
+    [SerializeField] GameObject objCollider;
+
+    //TODO: Get Player and Camera position control.
+
+    private float timeCounter = 0.0f;
+    private bool triggered = false;
 
 	// Update is called once per frame
 	void Update ()
     {
+        if(triggered)
+        {
+            timeCounter += Time.deltaTime;
+            if(timeCounter >= timeToLive)
+            {
+                graphic.SetActive(false);
 
+            }
+            //TODO: Add play animation when ready
+
+        //Freeze player controls
+        //Todo Camara focus on animation
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            Destroy(this.gameObject);
-
+            objCollider.SetActive(true); 
+            triggered = true;
         }
     }
 
