9431619ec520e0b16f0c7d35231bad507afdd46a
Fernando Diago <fernandodl2435@gmail.com>
2017-12-14 16:46:11 +0100

    Created Title Screen
    Title screen with pause panel, to change options

--- Assets/Scripts/Masters/SceneMaster.cs
+++ Assets/Scripts/Masters/SceneMaster.cs
@@ -1,8 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneMaster : MonoBehaviour
 {
-    
+    public void LoadLogoScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGameplayScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+
+    }
+
+    public void OpenMenuAnimation(GameObject menu)
+    {
+        menu.gameObject.SetActive(true);
+    }
+
+    public void OpenMenu(GameObject menu)
+    {
+        menu.gameObject.SetActive(true);
+    }
+
+    public void CloseMenu(GameObject menu)
+    {
+        menu.gameObject.SetActive(false);
+    }
 }
