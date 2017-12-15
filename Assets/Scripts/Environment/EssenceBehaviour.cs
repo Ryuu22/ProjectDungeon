364cc8982179defd11bf887b66961397b457eca8
ryuu <daniicarmona22@gmail.com>
2017-12-15 19:11:58 +0100

    Worked on essences.

---
+++ Assets/Scripts/Environment/EssenceBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EssenceBehaviour : MonoBehaviour {
+
+    [SerializeField] private float radius;
+
+    [Header("Game Elements")]
+    MoveMaster moveM;
+
+    Transform playerTransform;
+    Vector2 playerPos;
+    public float speed;
+
+    // Use this for initialization
+    void Start ()
+    {
+            
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveM.Move(this.gameObject, playerPos, speed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, radius);
+    }
+}
