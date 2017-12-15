6fe59e31dcea743dd4d7e26de10aca2b96f03c15
Fernando Diago <fernandodl2435@gmail.com>
2017-12-15 17:02:17 +0100

    commit to pull

--- Assets/Scripts/Masters/SceneMaster.cs
+++ Assets/Scripts/Masters/SceneMaster.cs
@@ -2,22 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneMaster : MonoBehaviour
 {
     [SerializeField]
-    GameObject blackScreen;
+    Image blackScreen;
+    [SerializeField]
+    Image logo;
+    public float logoFade;
+    public float fade;
+    public bool fadeOut = false;
+    public bool titleScreen = false;
 
-    float counter;
+    public float counter;
     float alphaCounter;
+
+    private void Start()
+    {
+        counter = 0;
+        fade = 1;
+        blackScreen.color = new Color(0, 0, 0, fade);
+    }
 
     private void Update()
     {
         counter += Time.deltaTime;
-        if (counter >= 5)
+
+        if(counter >= 3 && logo != null)
         {
-            
+            if(logoFade < 1) logoFade += Time.deltaTime / 3;
+
+            if(counter >= 8) FadeOut(false);
+
+            logo.color = new Color(0, 0, 0, logoFade);
         }
+
+        if (fade > 0 && !fadeOut) fade -= Time.deltaTime / 3;
+
+        blackScreen.color = new Color(0, 0, 0, fade);
+
+        if(fadeOut)
+        {
+
+            if(fade < 1) fade += Time.deltaTime / 2;
+
+            blackScreen.color = new Color(0, 0, 0, fade);
+
+            if(fade >= 1 && !titleScreen)
+            {
+                LoadTitleScreen();
+            }
+
+            if(fade >= 1 && titleScreen)
+            {
+                LoadGameplayScreen();
+            }
+        }
+
+    }
+
+    public void FadeOut(bool isTitleScreen)
+    {
+        fadeOut = true;
+        titleScreen = isTitleScreen;
     }
 
     public void LoadLogoScreen()
