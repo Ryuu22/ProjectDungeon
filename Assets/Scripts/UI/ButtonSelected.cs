80ef805d8bd957df9e6bda8c4d1d12a8337cce8b
Fernando Diago <fernandodl2435@gmail.com>
2017-12-15 23:33:01 +0100

    Swords Animations for title
    Buttons with blood and sword pointing to them when mouse is over the button!!

---
+++ Assets/Scripts/UI/ButtonSelected.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSelected : MonoBehaviour
+{
+    [SerializeField]
+    Animator myAnim;
+    
+    public void NewGame()
+    {
+        myAnim.SetTrigger("NewGame");
+    }
+
+    public void Options()
+    {
+        myAnim.SetTrigger("Options");
+    }
+
+    public void Exit()
+    {
+        myAnim.SetTrigger("Exit");
+    }
+
+    public void Nothing()
+    {
+        myAnim.SetTrigger("Nothing");
+    }
+
+}
