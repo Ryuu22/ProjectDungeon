1f4a4396491d18afbd956930dcab0ff03e733d25
Ryuu <daniicarmona22@gmail.com>
2018-01-29 18:48:46 +0100

    Added color variant.

---
+++ Assets/Scripts/Environment/TilesBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesBehaviour : MonoBehaviour {
+
+    public Color randomColor;
+    public float randomNumber;
+
+	// Use this for initialization
+	void Start ()
+    {
+        randomNumber = Random.Range(0.7f, 1.0f);
+
+        randomColor = new Color(randomNumber, randomNumber, randomNumber, 255);
+
+        this.gameObject.GetComponent<SpriteRenderer>().color = randomColor;
+        
+	}
+	
+
+}
