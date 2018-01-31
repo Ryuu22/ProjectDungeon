394654415ea14bd1c025bc359c57c37fcbe2d28a
Daniel Carmona <DaniiCarmona22@gmail.com>
2018-01-31 22:16:06 +0100

    Generated Automatic corridor generator

---
+++ Assets/Scripts/Environment/GroundGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour {
+
+    [Header("Rooms")]
+    public GameObject initialRoom;
+    public GameObject bossRoom;
+
+    [Header("Corridors")]
+    public GameObject[] Corridors;
+
+    public int length;
+    private Vector3 newCorridorPosition;
+
+	void Start ()
+    {
+        //Generate InitialRoom
+        initialRoom.gameObject.transform.position = Vector3.zero;
+        Instantiate(initialRoom);
+        //Generate EveryRoom
+        newCorridorPosition = Vector3.zero;
+
+        for(int i = 0; i < length; i++)
+        {
+            int randomInteger = Random.Range(0,Corridors.Length);
+            Corridors[randomInteger].transform.position = newCorridorPosition;
+            Instantiate(Corridors[randomInteger]);
+            newCorridorPosition.x += 12;
+        }
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
