dabf5d8ee9b4a8a558a143f3a0046295325d16eb
Daniel Carmona <DaniiCarmona22@gmail.com>
2018-02-12 19:12:08 +0100

    Finished random generation system

--- Assets/Scripts/Environment/GroundGenerator.cs
+++ Assets/Scripts/Environment/GroundGenerator.cs
@@ -13,26 +13,38 @@ public class GroundGenerator : MonoBehaviour {
 
     public int length;
     private Vector3 newCorridorPosition;
-
+    private int lastCorridor;
 	void Start ()
     {
         //Generate InitialRoom
         initialRoom.gameObject.transform.position = Vector3.zero;
         Instantiate(initialRoom);
         //Generate EveryRoom
+        lastCorridor = 0;
         newCorridorPosition = Vector3.zero;
 
         for(int i = 0; i < length; i++)
         {
             int randomInteger = Random.Range(0,Corridors.Length);
+            while(randomInteger == lastCorridor)
+            {
+                randomInteger = Random.Range(0, Corridors.Length);
+            }
             Corridors[randomInteger].transform.position = newCorridorPosition;
+
             Instantiate(Corridors[randomInteger]);
+
             newCorridorPosition.x += 12;
+
+            lastCorridor = randomInteger;
         }
+        //Generate BossRoom
+        bossRoom.gameObject.transform.position = newCorridorPosition;
+        Instantiate(bossRoom);
 
 
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
