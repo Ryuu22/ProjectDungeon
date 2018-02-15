9e7a8eedbe8d5f9bff3652d7e2e873068c339eb7
ryuu <daniicarmona22@gmail.com>
2018-02-15 18:53:57 +0100

    flying enemy done

--- Assets/Scripts/Enemies/Fly.cs
+++ Assets/Scripts/Enemies/Fly.cs
@@ -8,16 +8,33 @@ public class Fly : MonoBehaviour {
     MoveMaster moveM;
     Animator myAnim;
 
+    [Header("Stats")]
+
+    public float timeToLive;
+
     public float distanceFront;
     public float distanceBack;
     public float speed;
+
+    [Header("BodyParts")]
+
+    public GameObject mainSprite;
+    public GameObject[] bodyParts;
+
+    [SerializeField]bool dead;
+
+    float counter;
+
+
+
+    [Range(1,3)]public int damage;
 
 	// Use this for initialization
 	void Start ()
     {
        front = true;
        moveM = FindObjectOfType<MoveMaster>();
-        myAnim = this.GetComponentInChildren<Animator>();
+       myAnim = this.GetComponentInChildren<Animator>();
 
        	
 	}
@@ -25,12 +42,42 @@ public class Fly : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        myAnim.SetBool("FlyingFront", front);
+        if(!dead)
+        {
+            myAnim.SetBool("FlyingFront", front);
 
-        if (front)moveM.Move(this.gameObject,new Vector3(this.transform.position.x,this.transform.position.y,distanceFront), speed);
-        else moveM.Move(this.gameObject,new Vector3(this.transform.position.x,this.transform.position.y,distanceBack), speed);
+            if (front)moveM.Move(this.gameObject,new Vector3(this.transform.position.x,this.transform.position.y,distanceFront), speed);
+            else moveM.Move(this.gameObject,new Vector3(this.transform.position.x,this.transform.position.y,distanceBack), speed);
 
-        if (this.transform.position.z == distanceFront) front = false;
-        if (this.transform.position.z == distanceBack) front = true;
+            if (this.transform.position.z == distanceFront) front = false;
+            if (this.transform.position.z == distanceBack) front = true;
+        }
+        else
+        {
+            counter += Time.deltaTime;
+            if(counter > timeToLive)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == ("Player") && !dead)
+        {
+            myAnim.SetTrigger("Attack");
+            other.GetComponent<Player>().RecieveDamage(damage);
+        }
+    }
+    public void DieInstantly()
+    {
+        mainSprite.gameObject.SetActive(false);
+        for(int i = 0; i < bodyParts.Length; i++)
+        {
+            bodyParts[i].gameObject.SetActive(true);
+        }
+        dead = true;
+
     }
 }
